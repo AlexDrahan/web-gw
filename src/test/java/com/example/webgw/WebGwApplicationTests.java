0344cf1492a081b3d1734cc9df6df74fc8f2385e
OleksandrDrahan <dragan.sasha2002@gmail.com>
2023-04-06 11:40:58 +0300

    Creation a Spring Boot project

---
+++ src/test/java/com/example/webgw/WebGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
